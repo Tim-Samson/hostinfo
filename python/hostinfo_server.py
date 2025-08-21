4e71dfae6f9b619c8745f29bf0d75dbd31543f84
Tim-Samson <Timothy.L.Samson@ssa.gov>
2025-08-21 13:05:03 -0400

    try auto run

--- python/hostinfo_server.py
+++ python/hostinfo_server.py
@@ -2,7 +2,7 @@
 
 """
 A simple program that displays the hostname and IP
-Great for testing service routing in Kubernetes
+Great for testing service routing in Kubernetes - test
 """
 
 from datetime import datetime
